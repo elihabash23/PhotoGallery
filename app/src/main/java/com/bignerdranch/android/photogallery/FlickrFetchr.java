b2d79080be1685ee6d0e140e107aff4c7c05d01e
Elias Habash <eliashabash@aol.com>
2017-07-25 20:30:17 -0700

    Added the permission to use the INTERNET in the manifest

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -1,10 +1,53 @@
 package com.bignerdranch.android.photogallery;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by eliballislife11 on 7/25/17.
  */
 
 public class FlickrFetchr {
 
+    /**
+     * getUrlBytes(String) method fetches raw data from a URL and
+     * returns it as an array of bytes
+     */
 
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                        ": with " + urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    /**
+     * getUrl(String) method converts the result from
+     * getUrlBytes(String) to a String
+     */
+
+    public String getUrl(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
 }
