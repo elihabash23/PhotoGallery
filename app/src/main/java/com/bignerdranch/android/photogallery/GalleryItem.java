0ebdfa2fad0d191e5c3c3b5d6362dabdd0d1ca39
Elias Habash <eliashabash@aol.com>
2017-07-27 17:42:08 -0700

    Created the GalleryItem class to be the model object class

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.photogallery;
+
+/**
+ * Created by eliballislife11 on 7/27/17.
+ */
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -49,12 +49,13 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         protected Void doInBackground(Void... params) {
-            try {
-                String result = new FlickrFetchr().getUrl("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            } catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+//            try {
+//                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
+//                Log.i(TAG, "Fetched contents of URL: " + result);
+//            } catch (IOException ioe) {
+//                Log.e(TAG, "Failed to fetch URL: ", ioe);
+//            }
+            new FlickrFetchr().fetchItems();
             return null;
         }
     }
