7a037ead10ab32067520e582544391c08bf8daa2
Elias Habash <eliashabash@aol.com>
2017-08-04 00:51:02 -0700

    Added Messages, Loopers, and Handlers

--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
@@ -1,7 +1,15 @@
 package com.bignerdranch.android.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by eliballislife11 on 7/31/17.
@@ -9,11 +17,40 @@ import android.util.Log;
 
 public class ThumbnailDownloader<T> extends HandlerThread {
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;      // The what field for a message
 
     private boolean mHasQuit = false;
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
+    private Handler mResponseHandler;   // Holds a handler passed from the main thread
+    private ThumbnailDownloaderListener<T> mThumbnailDownloaderListener;
 
-    public ThumbnailDownloader() {
+    public interface ThumbnailDownloaderListener<T> {
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    }
+
+    public void setThumbnailDownloaderListener(ThumbnailDownloaderListener<T> listener) {
+        mThumbnailDownloaderListener = listener;
+    }
+
+
+    public ThumbnailDownloader(Handler responseHandler) {
         super(TAG);
+        mResponseHandler = responseHandler;
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for a URL: " + mRequestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
     }
 
     @Override
@@ -22,7 +59,53 @@ public class ThumbnailDownloader<T> extends HandlerThread {
         return super.quit();
     }
 
+    /**
+     * queueThumbnail obtains and sends messages.
+     * @param target is the PhotoHolder
+     * @param url the URL of the thumbnail photo
+     */
+
     public void queueThumbnail(T target, String url) {
         Log.i(TAG, "Got a URL: " + url);
+
+        if(url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
+        }
+    }
+
+    /**
+     * Helper function for onLooperPrepared()
+     * @param target
+     */
+
+    private void handleRequest(final T target) {
+        try {
+            final String url = mRequestMap.get(target);
+
+            if(url == null) {
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+            mResponseHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mRequestMap.get(target) != url || mHasQuit) {
+                        return;
+                    }
+
+                    mRequestMap.remove(target);
+                    mThumbnailDownloaderListener.onThumbnailDownloaded(target, bitmap);
+                }
+            });
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error downloading image", ioe);
+        }
     }
 }
