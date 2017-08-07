b90d3763d657b78bd1c7c71a1fe72c60341dc3ea
Elias Habash <eliashabash@aol.com>
2017-08-06 20:29:52 -0700

    Added code to verify if a network is available for networking in the background

--- app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.photogallery;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /**
@@ -28,6 +29,24 @@ public class PollService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+
         Log.i(TAG, "Received an intent: " + intent);
+    }
+
+    /**
+     * This method verifies with the ConnectivityManager that the network is
+     * available since networking is being done in the background
+     * @return
+     */
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
     }
 }
