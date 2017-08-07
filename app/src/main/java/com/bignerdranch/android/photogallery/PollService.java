7cb1063a1d92c2f150b9cecf52afc1356178ef21
Elias Habash <eliashabash@aol.com>
2017-08-06 21:30:04 -0700

    Added a service that polls Flickr for new results

--- app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.util.Log;
 
+import java.util.List;
+
 /**
  * Created by eliballislife11 on 8/5/17.
  */
@@ -27,14 +29,44 @@ public class PollService extends IntentService {
         super(TAG);
     }
 
+
     @Override
     protected void onHandleIntent(Intent intent) {
         if (!isNetworkAvailableAndConnected()) {
             return;
         }
 
-        Log.i(TAG, "Received an intent: " + intent);
+//        Log.i(TAG, "Received an intent: " + intent);
+//        This code will use a service that will poll for new results
+//         Step 1: Pull out the query and the last result ID from the default SharedPreferences
+//         Step 2: Fetch the latest result with FlickrFetchr
+//         Step 3: If there are results, grab the first one
+//         Step 4: Check to see whether it is different from the last result ID
+//         Step 5: Store the first result back in SharedPreferences
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if (query == null) {
+            items = new FlickrFetchr().fetchRecentPhotos();
+        } else {
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        if (items.size() == 0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
     }
+
 
     /**
      * This method verifies with the ConnectivityManager that the network is
--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
@@ -9,6 +9,7 @@ import android.preference.PreferenceManager;
 
 public class QueryPreferences {
     private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID = "lastResultId";
 
     /**
      * getStoredQuery returns the query value stored in shared preferences
@@ -32,4 +33,17 @@ public class QueryPreferences {
                 .putString(PREF_SEARCH_QUERY, query)
                 .apply();
     }
+
+    public static String getLastResultId(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_LAST_RESULT_ID, null);
+    }
+
+    public static void setLastResultId(Context context, String lastResultId) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_LAST_RESULT_ID, lastResultId)
+                .apply();
+    }
+
 }
